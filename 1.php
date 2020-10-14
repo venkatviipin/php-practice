bd6dc7982e4874b3ab338fe5b59f2d6e55c7d6d2
venkatviipin <venkatz1998@gmail.com>
2020-10-14 12:44:50 +0530

    Second Commit commenting line

--- 1.php
+++ 1.php
@@ -18,15 +18,6 @@
 		<?php  
 
 
-		/* 
-		   Step 1:  Use the Echo Function to say hello with html h1                  tags embedded inside php.
-
-
-		   Step 2: Write a comment above the echo function and explain
-		   what that function is doing.
-
-
-		   */
 
 		?>
 
